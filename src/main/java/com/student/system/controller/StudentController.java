f657d39be95463406b017d2ebea1226088a5204d
eperez <eperez@huenei.com>
2021-01-19 22:07:08 -0300

    changing request mapping in student controller

--- src/main/java/com/student/system/controller/StudentController.java
+++ src/main/java/com/student/system/controller/StudentController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/student")
+@RequestMapping("/student-2")
 public class StudentController {
 
     @Autowired
